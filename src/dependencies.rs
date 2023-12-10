3df093dbd9644205d0962c2a8c35886205f80c65
David Chubabriya <david.chubabriya@gmail.com>
2023-12-10 18:10:11 +0000

    Dependencies wrapped in Arc and RwLock

--- src/dependencies.rs
+++ src/dependencies.rs
@@ -1,7 +1,14 @@
+use std::sync::Arc;
+use tokio::sync::RwLock;
+
+#[derive(Default)]
 struct URLFrontier {}
+
+#[derive(Default)]
 struct DataStore {}
 
+#[derive(Default)]
 pub struct Dependencies {
-    url_frontier: URLFrontier,
-    data_store: DataStore,
+    url_frontier: Arc<RwLock<URLFrontier>>,
+    data_store: Arc<RwLock<DataStore>>,
 }
