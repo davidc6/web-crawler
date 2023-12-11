b88fd7a11796df351b7d08a9cfad2aee010f1d70
David Chubabriya <david.chubabriya@gmail.com>
2023-12-11 20:04:17 +0000

    Dependencies struct wrapped in a single Arc and RwLock

--- src/dependencies.rs
+++ src/dependencies.rs
@@ -1,14 +1,22 @@
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
+pub type Deps = Arc<RwLock<Dependencies>>;
+
 #[derive(Default)]
 struct URLFrontier {}
 
 #[derive(Default)]
 struct DataStore {}
 
+impl Dependencies {
+    pub fn new() -> Deps {
+        Arc::new(RwLock::new(Dependencies::default()))
+    }
+}
+
 #[derive(Default)]
 pub struct Dependencies {
-    url_frontier: Arc<RwLock<URLFrontier>>,
-    data_store: Arc<RwLock<DataStore>>,
+    url_frontier: URLFrontier,
+    data_store: DataStore,
 }
