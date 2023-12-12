da9e943f1ac124968a5b8a60f8bf95a1b3a6eebc
David Chubabriya <david.chubabriya@gmail.com>
2023-12-12 07:11:39 +0000

    Update main to include deps

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::Parser as ClapParser;
 use env_logger::Env;
-use log::info;
+use log::{info, warn};
+use std::io::Error;
+use url_crawler::dependencies::{Dependencies, Deps};
 
 #[derive(ClapParser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -22,11 +24,26 @@ struct Args {
     print: bool,
 }
 
-fn main() {
+async fn execute(args: Args, deps: Deps) -> Result<(), Error> {
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
     // If RUST_LOG env is not set, fallback to printing all logs at info-level or above
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let cli_args = Args::parse();
 
     info!("Initialising with seed url: {}", cli_args.url);
+
+    let deps = Dependencies::new();
+    match execute(cli_args, deps).await {
+        Ok(_) => {
+            info!("Done");
+        }
+        Err(e) => {
+            warn!("There's been an error: {}", e)
+        }
+    }
 }
