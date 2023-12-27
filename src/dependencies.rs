1f0536651f272a937b0cfbe73fa21442de6bba72
David Chubabriya <david.chubabriya@gmail.com>
2023-12-27 19:14:28 +0000

    Update Dependencies module

--- src/dependencies.rs
+++ src/dependencies.rs
@@ -1,22 +1,43 @@
+use crate::url_frontier::URLFrontier;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
-pub type Deps = Arc<RwLock<Dependencies>>;
+pub type Deps<T> = Arc<RwLock<Dependencies<T>>>;
 
 #[derive(Default)]
-struct URLFrontier {}
+pub struct DataStore {}
 
-#[derive(Default)]
-struct DataStore {}
+impl<T: Default> Dependencies<T> {
+    pub fn new() -> Dependencies<T> {
+        Dependencies::default()
+    }
 
-impl Dependencies {
-    pub fn new() -> Deps {
-        Arc::new(RwLock::new(Dependencies::default()))
+    pub fn url_frontier(self, url_frontier: URLFrontier<T>) -> Dependencies<T> {
+        let Self { data_store, .. } = self;
+        Dependencies {
+            url_frontier,
+            data_store,
+        }
+    }
+
+    pub fn data_store(self, data_store: DataStore) -> Dependencies<T> {
+        let Self { url_frontier, .. } = self;
+        Dependencies {
+            url_frontier,
+            data_store,
+        }
+    }
+
+    pub fn build(self) -> Arc<RwLock<Dependencies<T>>> {
+        Arc::new(RwLock::new(Self {
+            url_frontier: self.url_frontier,
+            data_store: self.data_store,
+        }))
     }
 }
 
 #[derive(Default)]
-pub struct Dependencies {
-    url_frontier: URLFrontier,
-    data_store: DataStore,
+pub struct Dependencies<T> {
+    pub url_frontier: URLFrontier<T>,
+    pub data_store: DataStore,
 }
