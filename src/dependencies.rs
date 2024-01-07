1d6c2b166278eb7ae2b513d3b742c71f20edcd9a
David Chubabriya <david.chubabriya@gmail.com>
2024-01-07 12:37:11 +0000

    Update dependencies

--- src/dependencies.rs
+++ src/dependencies.rs
@@ -1,19 +1,29 @@
-use crate::{data_store::Store, url_frontier::URLFrontier};
+use crate::{
+    data_store::{DataStore, Store},
+    url_frontier::{Queue, URLFrontier},
+};
 use std::{fmt::Debug, hash::Hash, sync::Arc};
 use tokio::sync::RwLock;
 
 pub type Deps<T, U> = Arc<RwLock<Dependencies<T, U>>>;
-pub type DepsConcrete = Arc<RwLock<Dependencies<String, String>>>;
+pub type DepsConcrete = Arc<Dependencies<String, String>>;
 
-#[derive(Default)]
-pub struct DataStore {}
-
-impl<T: Clone + Default + Hash + Eq, U: Debug + Default> Dependencies<T, U> {
+impl<
+        T: Clone + Default + Debug + Hash + Eq + Send + Sync + 'static,
+        U: Send + Debug + Sync + Default + 'static,
+    > Dependencies<T, U>
+{
     pub fn new() -> Dependencies<T, U> {
-        Dependencies::default()
+        Dependencies {
+            url_frontier: Arc::new(RwLock::new(URLFrontier::default())),
+            data_store: Arc::new(RwLock::new(Store::default())),
+        }
     }
 
-    pub fn url_frontier(self, url_frontier: URLFrontier<T>) -> Dependencies<T, U> {
+    pub fn url_frontier(
+        self,
+        url_frontier: Arc<RwLock<dyn Queue<T> + Send + Sync>>,
+    ) -> Dependencies<T, U> {
         let Self { data_store, .. } = self;
         Dependencies {
             url_frontier,
@@ -21,7 +31,10 @@ impl<T: Clone + Default + Hash + Eq, U: Debug + Default> Dependencies<T, U> {
         }
     }
 
-    pub fn data_store(self, data_store: Store<T, U>) -> Dependencies<T, U> {
+    pub fn data_store(
+        self,
+        data_store: Arc<RwLock<dyn DataStore<T, U> + Send + Sync>>,
+    ) -> Dependencies<T, U> {
         let Self { url_frontier, .. } = self;
         Dependencies {
             url_frontier,
@@ -29,16 +42,16 @@ impl<T: Clone + Default + Hash + Eq, U: Debug + Default> Dependencies<T, U> {
         }
     }
 
-    pub fn build(self) -> Arc<RwLock<Dependencies<T, U>>> {
-        Arc::new(RwLock::new(Self {
+    pub fn build(self) -> Arc<Dependencies<T, U>> {
+        Arc::new(Self {
             url_frontier: self.url_frontier,
             data_store: self.data_store,
-        }))
+        })
     }
 }
 
-#[derive(Default)]
+// #[derive(Default)]
 pub struct Dependencies<T: Clone + Hash + Eq, U> {
-    pub url_frontier: URLFrontier<T>,
-    pub data_store: Store<T, U>,
+    pub url_frontier: Arc<RwLock<dyn Queue<T> + Send + Sync>>,
+    pub data_store: Arc<RwLock<dyn DataStore<T, U> + Send + Sync>>,
 }
