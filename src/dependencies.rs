ea3d5cdf06aa68a8cc3130ab56c1fbc760bdcd3b
David Chubabriya <david.chubabriya@gmail.com>
2024-01-12 19:11:49 +0000

    Update and experiment with dependencies

--- src/dependencies.rs
+++ src/dependencies.rs
@@ -1,29 +1,53 @@
 use crate::{
     data_store::{DataStore, Store},
-    url_frontier::{Queue, URLFrontier},
+    url_frontier::{Queue, URLFrontier, URLFrontierBuilder},
 };
-use std::{fmt::Debug, hash::Hash, sync::Arc};
+use std::{default, fmt::Debug, hash::Hash, sync::Arc};
 use tokio::sync::RwLock;
 
 pub type Deps<T, U> = Arc<RwLock<Dependencies<T, U>>>;
 pub type DepsConcrete = Arc<Dependencies<String, String>>;
+
+pub struct Frontier<T>(pub Arc<RwLock<dyn Queue<T> + Send + Sync>>);
+
+pub struct Options {
+    pub delay_s: Option<u64>,
+    pub uri: String,
+}
+
+pub fn q<T: Send + Sync + Default + 'static>(opts: Options) -> Frontier<T> {
+    // let u = URLFrontierBuilder::new()
+    //     .delay_s(opts.delay_s.unwrap())
+    //     .value(opts.uri)
+    //     .build();
+
+    let mut u = URLFrontier::default();
+    u.set_delay(opts.delay_s);
+
+    Frontier(Arc::new(RwLock::new(u)))
+}
+
+// trait Depos {
+//     type D: DataStore;
+//     type F: Queue;
+
+//     fn f(&self) -> &Self::F;
+//     fn d(&self) -> &Self::D;
+// }
 
 impl<
         T: Clone + Default + Debug + Hash + Eq + Send + Sync + 'static,
         U: Send + Debug + Sync + Default + 'static,
     > Dependencies<T, U>
 {
-    pub fn new() -> Dependencies<T, U> {
+    pub fn new(opts: Options) -> Dependencies<T, U> {
         Dependencies {
-            url_frontier: Arc::new(RwLock::new(URLFrontier::default())),
+            url_frontier: q(opts),
             data_store: Arc::new(RwLock::new(Store::default())),
         }
     }
 
-    pub fn url_frontier(
-        self,
-        url_frontier: Arc<RwLock<dyn Queue<T> + Send + Sync>>,
-    ) -> Dependencies<T, U> {
+    pub fn url_frontier(self, url_frontier: Frontier<T>) -> Dependencies<T, U> {
         let Self { data_store, .. } = self;
         Dependencies {
             url_frontier,
@@ -50,8 +74,7 @@ impl<
     }
 }
 
-// #[derive(Default)]
 pub struct Dependencies<T: Clone + Hash + Eq, U> {
-    pub url_frontier: Arc<RwLock<dyn Queue<T> + Send + Sync>>,
+    pub url_frontier: Frontier<T>,
     pub data_store: Arc<RwLock<dyn DataStore<T, U> + Send + Sync>>,
 }
