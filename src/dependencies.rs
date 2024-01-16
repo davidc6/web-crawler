8aaf88ac0ae8bf4713bcb865bdb64e99bc188a1b
David Chubabriya <david.chubabriya@gmail.com>
2024-01-16 16:01:11 +0000

    Use newtype pattern to inject url_frontier dependency

--- src/dependencies.rs
+++ src/dependencies.rs
@@ -1,48 +1,49 @@
 use crate::{
     data_store::{DataStore, Store},
-    url_frontier::{Queue, URLFrontier, URLFrontierBuilder},
+    url_frontier::{self, Queue, URLFrontier, URLFrontierBuilder},
 };
-use std::{default, fmt::Debug, hash::Hash, sync::Arc};
+use std::{fmt::Debug, hash::Hash, ops::Deref, sync::Arc};
 use tokio::sync::RwLock;
 
 pub type Deps<T, U> = Arc<RwLock<Dependencies<T, U>>>;
 pub type DepsConcrete = Arc<Dependencies<String, String>>;
 
+// Implement the Deref trair in order to access impl Queue without having to .0
+impl<T> Deref for Frontier<T> {
+    type Target = Arc<RwLock<dyn Queue<T> + Send + Sync>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 pub struct Frontier<T>(pub Arc<RwLock<dyn Queue<T> + Send + Sync>>);
 
-pub struct Options {
+#[derive(Default)]
+pub struct UrlFrontierOptions<T> {
     pub delay_s: Option<u64>,
-    pub uri: String,
+    pub uri: T,
 }
 
-pub fn q<T: Send + Sync + Default + 'static>(opts: Options) -> Frontier<T> {
-    // let u = URLFrontierBuilder::new()
-    //     .delay_s(opts.delay_s.unwrap())
-    //     .value(opts.uri)
-    //     .build();
+pub fn url_frontier<T: Send + Sync + Default + 'static>(
+    opts: UrlFrontierOptions<T>,
+) -> Frontier<T> {
+    let url_frontier = URLFrontierBuilder::default()
+        .delay_s(opts.delay_s.unwrap())
+        .value(opts.uri)
+        .build();
 
-    let mut u = URLFrontier::default();
-    u.set_delay(opts.delay_s);
-
-    Frontier(Arc::new(RwLock::new(u)))
+    Frontier(Arc::new(RwLock::new(url_frontier)))
 }
-
-// trait Depos {
-//     type D: DataStore;
-//     type F: Queue;
-
-//     fn f(&self) -> &Self::F;
-//     fn d(&self) -> &Self::D;
-// }
 
 impl<
         T: Clone + Default + Debug + Hash + Eq + Send + Sync + 'static,
         U: Send + Debug + Sync + Default + 'static,
     > Dependencies<T, U>
 {
-    pub fn new(opts: Options) -> Dependencies<T, U> {
+    pub fn new() -> Dependencies<T, U> {
         Dependencies {
-            url_frontier: q(opts),
+            url_frontier: url_frontier(UrlFrontierOptions::default()),
             data_store: Arc::new(RwLock::new(Store::default())),
         }
     }
